7bced40bc4c59f3e3c83ef12a6ab0d7ceb0f7ed5
Dania <Dania2008@list.ru>
2022-09-29 20:16:25 +0300

    Added task 27

---
+++ Task027/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт
+// сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] a = new int[n];
+int b = 0;
+for(int i = 0; i < a.Length; i ++)
+{
+    a[i] = n % 10;
+    b = b + a[i];
+    n = n / 10;
+}
+Console.WriteLine(b);
