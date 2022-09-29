ffc3d9978ecf33713b4e1b372cfd262f9f18b933
Dania <Dania2008@list.ru>
2022-09-29 20:19:04 +0300

    Added task 29

---
+++ Task029/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+Console.WriteLine("Введите последовательно восемь чисел, каждое с новой строки: ");
+int []array = new int[8];
+
+for (int i = 0;i < array.Length; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(", ", array) + "]");
